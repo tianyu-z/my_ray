f8eaac7e4d206c6779a664ca6bebc113759f0421
tianyu-z <zhangtianyupro@gmail.com>
2023-04-20 16:18:39 -0400

    update

--- rllib/examples/env/utils/mixins.py
+++ rllib/examples/env/utils/mixins.py
@@ -41,6 +41,57 @@ class TwoPlayersTwoActionsInfoMixin(InfoAccumulationInterface, ABC):
         self.dd_count.append(ac0 == 1 and ac1 == 1)
 
 
+class MessagedTwoPlayersTwoActionsInfoMixin(InfoAccumulationInterface, ABC):
+    """
+    Mixin class to add logging capability in a two player discrete game.
+    Logs the frequency of each state.
+    """
+
+    def _init_info(self):
+        self.cc_count = []
+        self.dd_count = []
+        self.cd_count = []
+        self.dc_count = []
+        self.turth_telling_count = []
+        self.promise_breaking_count = []
+
+    def _reset_info(self):
+        self.cc_count.clear()
+        self.dd_count.clear()
+        self.cd_count.clear()
+        self.dc_count.clear()
+        self.turth_telling_count.clear()
+        self.promise_breaking_count.clear()
+
+    def _get_episode_info(self):
+        return {
+            "CC": np.mean(self.cc_count).item(),
+            "DD": np.mean(self.dd_count).item(),
+            "CD": np.mean(self.cd_count).item(),
+            "DC": np.mean(self.dc_count).item(),
+            # "Truth": np.mean(self.turth_telling_count).item(),
+            # "PromiseBreak": np.mean(self.promise_breaking_count).item(),
+        }
+
+    def _accumulate_info(self, ac0, ac1, NUM_ACTIONS=2):
+        actual_action_player_0 = ac0 // NUM_ACTIONS
+        actual_action_player_1 = ac1 // NUM_ACTIONS
+        message_action_player_0 = ac0 % NUM_ACTIONS
+        message_action_player_1 = ac1 % NUM_ACTIONS
+        self.cc_count.append(
+            actual_action_player_0 == 0 and actual_action_player_1 == 0
+        )
+        self.cd_count.append(
+            actual_action_player_0 == 0 and actual_action_player_1 == 1
+        )
+        self.dc_count.append(
+            actual_action_player_0 == 1 and actual_action_player_1 == 0
+        )
+        self.dd_count.append(
+            actual_action_player_0 == 1 and actual_action_player_1 == 1
+        )
+
+
 class NPlayersNDiscreteActionsInfoMixin(InfoAccumulationInterface, ABC):
     """
     Mixin class to add logging capability in N player games with
---
+++ rllib/examples/iterated_prisoners_dilemma_env_customize.py
@@ -0,0 +1,135 @@
+##########
+# Contribution by the Center on Long-Term Risk:
+# https://github.com/longtermrisk/marltoolbox
+##########
+import argparse
+import os
+
+import ray
+from ray import air, tune
+from ray.rllib.algorithms.pg import PG
+from ray.rllib.algorithms.ppo import PPO
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--framework",
+    choices=["tf", "tf2", "torch"],
+    default="torch",
+    help="The DL framework specifier.",
+)
+parser.add_argument("--stop-iters", type=int, default=200)
+parser.add_argument("--algo", type=str, default="PPO", help="PG or PPO")
+parser.add_argument("--ismessaged", type=int, default=1, help="0 or 1")
+
+
+from ray.rllib.algorithms.callbacks import DefaultCallbacks
+import csv
+
+
+class CustomLoggingCallback(DefaultCallbacks):
+    def on_episode_end(self, worker, base_env, policies, episode, **kwargs):
+        env = base_env.get_sub_environments()[0]
+        info = env._get_episode_info()
+
+        for k, v in info.items():
+            episode.custom_metrics[k] = v
+        # Save custom metrics to a CSV file
+        episode_id = episode.episode_id
+        episode_step = episode.length
+        custom_metrics = episode.custom_metrics
+
+        with open("custom_metrics.csv", mode="a") as csv_file:
+            fieldnames = ["episode_id", "episode_step"] + list(custom_metrics.keys())
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            if csv_file.tell() == 0:
+                writer.writeheader()
+
+            row = {"episode_id": episode_id, "episode_step": episode_step}
+            row.update(custom_metrics)
+            writer.writerow(row)
+
+
+def main(debug, args):
+    # stop_iters, framework, algo_name = args.stop_iters, args.framework, args.algo
+    algo_name = args.algo
+    algo = globals()[algo_name]
+    train_n_replicates = 1 if debug else 1
+    seeds = list(range(train_n_replicates))
+
+    ray.init(num_cpus=os.cpu_count(), num_gpus=1, local_mode=debug)
+
+    rllib_config, stop_config = get_rllib_config(seeds, debug, args)
+    tuner = tune.Tuner(
+        algo,
+        param_space=rllib_config,
+        run_config=air.RunConfig(
+            name=f"{algo_name}_IPD",
+            stop=stop_config,
+            checkpoint_config=air.CheckpointConfig(
+                checkpoint_frequency=0,
+                checkpoint_at_end=True,
+            ),
+        ),
+    )
+    tuner.fit()
+    ray.shutdown()
+
+
+def get_rllib_config(seeds, debug=False, args=None):
+    assert args is not None
+    stop_config = {
+        "training_iteration": 2 if debug else args.stop_iters,
+    }
+
+    if args.ismessaged:
+        from ray.rllib.examples.env.matrix_sequential_social_dilemma_messaged import (
+            MessageIteratedPrisonersDilemma as IPD,
+        )
+    else:
+        from ray.rllib.examples.env.matrix_sequential_social_dilemma import (
+            IteratedPrisonersDilemma as IPD,
+        )
+
+    env_config = {
+        "players_ids": ["player_row", "player_col"],
+        "max_steps": 20,
+        "get_additional_info": True,
+    }
+
+    rllib_config = {
+        "env": IPD,
+        "env_config": env_config,
+        "multiagent": {
+            "policies": {
+                env_config["players_ids"][0]: (
+                    None,
+                    IPD.OBSERVATION_SPACE,
+                    IPD.ACTION_SPACE,
+                    {},
+                ),
+                env_config["players_ids"][1]: (
+                    None,
+                    IPD.OBSERVATION_SPACE,
+                    IPD.ACTION_SPACE,
+                    {},
+                ),
+            },
+            "policy_mapping_fn": lambda agent_id, episode, worker, **kwargs: agent_id,
+        },
+        "seed": tune.grid_search(seeds),
+        "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
+        "framework": args.framework,
+        # Add buffer settings for the PPO algorithm
+        "rollout_fragment_length": 200,
+        "train_batch_size": 4000,
+        "callbacks": CustomLoggingCallback,
+    }
+
+    return rllib_config, stop_config
+
+
+if __name__ == "__main__":
+    debug_mode = False
+    args = parser.parse_args()
+    main(debug_mode, args)
